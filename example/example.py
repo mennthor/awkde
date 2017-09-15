12eef0d9ecc36d3567f1b8f05c9e6d6ea42489be
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-09-15 12:28:39 +0200

    example :: Added json dump of example model and slighlty adapted example.

--- example/example.py
+++ example/example.py
@@ -1,35 +1,49 @@
 # coding: utf-8
 
+"""
+A 2D example for the adaptive width KDE.
+
+Sample x1 from a gaussian and x2 from x*exp(-x).
+Make a plot with the prediction and save the model to a JSON dump.
+"""
+
 import numpy as np
 import scipy.stats as scs
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.colors import LogNorm
 
-from awkde import GaussianKDE
+from awkde import GaussianKDE, json2kde
 
 
-# Choose distribution with different scales here
-rndgen = np.random.RandomState(seed=375005)  # ESEL
+rndgen = np.random.RandomState(seed=3575)  # ESEL
 # Gaussian
-logE_mean = 3.
-logE_sigma = .25
+mean = 3.
+sigma = .25
 # a^2 * x * exp(-a * x)
-sigma_scale = 100.
+a = 100.
 
-# Sample logE from gaussian, sigma from x*exp(-x)
 n_samples = 1000
-logE_sam = rndgen.normal(logE_mean, logE_sigma,
-                         size=n_samples)
+logE_sam = rndgen.normal(mean, sigma, size=n_samples)
+
 # From pythia8: home.thep.lu.se/~torbjorn/doxygen/Basics_8h_source.html
 u1, u2 = rndgen.uniform(size=(2, n_samples))
-sigma_sam = -np.log(u1 * u2) / sigma_scale
+sigma_sam = -np.log(u1 * u2) / a
+
 # Shape must be (n_points, n_features)
 sample = np.vstack((logE_sam, sigma_sam)).T
 
-# Create KDE and fit it
+# Create KDE and fit it. Save model in JSON format
+print("Fitting model to {} sample points.".format(n_samples))
 kde = GaussianKDE(glob_bw="silverman", alpha=0.5, diag_cov=True)
 kde.fit(sample)
+
+# Save and load the model
+outf = "./example_KDE.json"
+print("Saving model to {}".format(outf))
+kde.to_json(outf)
+print("Loading same model from {}".format(outf))
+kde = json2kde(outf)
 
 # Evaluate at dense grid
 minx, maxx = np.amin(sample[:, 0]), np.amax(sample[:, 0])
@@ -45,23 +59,22 @@ zz = kde.predict(grid_pts)
 ZZ = zz.reshape(XX.shape)
 
 
-# Evaluate truth at same grid
+# Evaluate true PDFs at same grid
 def fx(x):
-    return scs.norm.pdf(x, logE_mean, logE_sigma)
+    return scs.norm.pdf(x, mean, sigma)
 
 
 def fy(y):
-    a = sigma_scale
     return a**2 * y * np.exp(-a * y)
 
 
 fZ = (fx(grid_pts[:, 0]) * fy(grid_pts[:, 1])).reshape(XX.shape)
 
-# Sample new from KDE
+# Sample new points from KDE model
 kde_sam = kde.sample(n_samples=100000, random_state=rndgen)
 
-
-# Big plot on the left (2D KDE + points )and three right (1D margins + hist)
+print("Making example plot.")
+# Big plot on the left (2D KDE + points ) and three right (1D margins + hist)
 fig = plt.figure(figsize=(10, 6))
 gs = gridspec.GridSpec(3, 3)
 axl = fig.add_subplot(gs[:, :2])
@@ -92,13 +105,13 @@ axrb.set_title("True 1D PDF + KDE sample")
 for axi in (axl, axrt):
     axi.set_xlim(minx, maxx)
     axi.set_ylim(0, maxy)
-    axi.set_xlabel("x")
-    axi.set_ylabel("y")
+    axi.set_xlabel("x1")
+    axi.set_ylabel("x2")
 
 axrc.set_xlim(minx, maxx)
 axrb.set_xlim(miny, maxy)
-axrc.set_xlabel("x")
-axrb.set_xlabel("y")
+axrc.set_xlabel("x1")
+axrb.set_xlabel("x2")
 
 fig.tight_layout()
 fig.savefig("example.png", dpi=50)
